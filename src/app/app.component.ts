ee01169494111bd0ba7e20bac88d7c515b776b87
deepak <deepakarya0155@gmail.com>
2020-08-15 10:17:26 +0530

    animation added and git pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { trigger, transition, animate,style, keyframes } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger("fadein",[
+      
+      transition(":enter",[
+        animate('.5s',keyframes([
+          style({
+            transform:'translateX(4px)'
+          }),
+          style({
+            transform:'translateX(-4px)'
+          }),
+          style({
+            transform:'translateX(4px)'
+          }),
+          style({
+            transform:'translateX(-4px)'
+          }),
+          style({
+            transform:'translateX(4px)'
+          }),
+          style({
+            transform:'translateX(-4px)'
+          }),
+        ]))
+      ])
+    ])
+  ]
+
+  
+
 })
 export class AppComponent {
   title = 'FrontEnd';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NavBar} from './navbar/navbar'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,NavBar
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
